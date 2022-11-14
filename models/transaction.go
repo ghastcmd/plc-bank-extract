7ddeb074a1413ec5d320c9e2ef2b840bf51d95f9
GhastCMD <kraken32bin@protonmail.com>
2022-11-14 19:39:57 -0300

    feat(Models): modified transaction object to have a specifier for receiving or not

--- models/transaction.go
+++ models/transaction.go
@@ -1,13 +1,26 @@
 package models
 
+const (
+	CreditCard = iota
+	Pix
+	Transference
+)
+
 type Transaction struct {
 	Mode      int     `json:"mode"`
+	Receiving bool    `json:"receiving"`
 	Recipient string  `json:"recipient"`
 	Value     float64 `json:"value"`
 }
 
-func (t *Transaction) SetTransaction(mode int, recipient string, value float64) {
+func (t *Transaction) SetTransaction(
+	mode int,
+	is_receiving bool,
+	recipient string,
+	value float64,
+) {
 	t.Mode = mode
+	t.Receiving = is_receiving
 	t.Recipient = recipient
 	t.Value = value
 }
