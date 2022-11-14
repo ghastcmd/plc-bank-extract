1eb302e4e9f44174c1c0f08e5c1f308aa93e9716
GhastCMD <kraken32bin@protonmail.com>
2022-11-14 18:33:17 -0300

    feat(Models): added transaction functions

--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	model "github.com/ghastcmd/plc-bank-extract/models"
 )
 
 func main() {
 	fmt.Println("Hello world!")
-	model.HelloFromModel()
 }
--- models/transaction.go
+++ models/transaction.go
@@ -1,13 +1,13 @@
 package models
 
-import "fmt"
-
 type Transaction struct {
 	Mode      int     `json:"mode"`
 	Recipient string  `json:"recipient"`
 	Value     float64 `json:"value"`
 }
 
-func HelloFromModel() {
-	fmt.Println("Hello from <model> package.")
+func (t *Transaction) SetTransaction(mode int, recipient string, value float64) {
+	t.Mode = mode
+	t.Recipient = recipient
+	t.Value = value
 }
