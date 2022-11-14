25f55a854720e51abaa2549665409109e0dc55e7
GhastCMD <kraken32bin@protonmail.com>
2022-11-14 19:40:28 -0300

    feat(Models): added the methods specified before

--- models/current_day_extract.go
+++ models/current_day_extract.go
@@ -1,6 +1,8 @@
 package models
 
-import "time"
+import (
+	"time"
+)
 
 type DayExtract struct {
 	Date              time.Time     `json:"date"`
@@ -8,4 +10,35 @@ type DayExtract struct {
 	TransactionInput  []Transaction `json:"[]transactionInput"`
 	OutputValue       float64       `json:"outputValue"`
 	TransactionOutput []Transaction `json:"[]transactionOutput"`
+}
+
+func (de *DayExtract) GetDayExtract(transactions []Transaction) string {
+	// Separate all the transactions into input and output transaction
+	for _, transaction := range transactions {
+		if transaction.Receiving {
+			de.TransactionInput = append(de.TransactionInput, transaction)
+		} else {
+			de.TransactionOutput = append(de.TransactionOutput, transaction)
+		}
+	}
+
+	// calculate both the input and output differente within transactions
+	de.InputValue = de.calculateInputValue()
+	de.OutputValue = de.calculateOutputValue()
+
+	return "status(200)"
+}
+
+func (de *DayExtract) calculateInputValue() (outValue float64) {
+	for _, transaction := range de.TransactionInput {
+		outValue += transaction.Value
+	}
+	return outValue
+}
+
+func (de *DayExtract) calculateOutputValue() (inValue float64) {
+	for _, transaction := range de.TransactionInput {
+		inValue += transaction.Value
+	}
+	return inValue
 }
